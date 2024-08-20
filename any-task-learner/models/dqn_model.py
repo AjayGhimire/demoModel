a5a8ab4515f19086ff3f280e626e63bb082570be
AjayGhimire <ajaghi2357@invenaut.de>
2024-08-20 09:08:52 +0200

    New changes

--- any-task-learner/models/dqn_model.py
+++ any-task-learner/models/dqn_model.py
@@ -1,16 +1,15 @@
 import tensorflow as tf
 from tensorflow.keras import layers
-from .base_model import BaseModel
 
-class DQN(BaseModel):
+class DQN(tf.keras.Model):
     def __init__(self, state_dim, action_dim):
         super(DQN, self).__init__()
         self.fc1 = layers.Dense(128, activation='relu')
         self.fc2 = layers.Dense(128, activation='relu')
-        self.q_values = layers.Dense(action_dim, activation='linear')
+        self.output_layer = layers.Dense(action_dim, activation='linear')
 
     def call(self, state):
         x = self.fc1(state)
         x = self.fc2(x)
-        q_values = self.q_values(x)
+        q_values = self.output_layer(x)
         return q_values
